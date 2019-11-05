94fb4be13ad4ed96d1ab968151192ac5fc8dd5dd
Lewis Barnett <labarnett@ucdavis.edu>
2019-11-04 16:46:38 -0800

    init

---
+++ run_sdmTMB_WC.R
@@ -0,0 +1,177 @@
+# Fitting spatiotemporal models to West Coast Groundfish Bottom Trawl data with sdmTMB
+# TO DO: further standardization with other regions regarding projections and prediction grid resolution
+
+#devtools::install_github("pbs-assess/sdmTMB")
+#library(INLA) # if we want tools to make other meshes
+library(ggplot2)
+library(raster)
+library(dplyr)
+library(sdmTMB)
+library(sp)
+
+###########################################################################################################
+# options 
+
+# specify # of knots for mesh
+n_knots = 750
+
+# do cross validation?
+use_cv = FALSE
+
+###########################################################################################################
+# Prepare data and fit models
+
+# haul data includes environmental covariates with location information
+haul = nwfscSurvey::PullHaul.fn(SurveyName = "NWFSC.Combo")
+
+# project lat/lon to UTM, after removing missing values and unsatisfactory hauls
+haul = haul %>% filter(!is.na(longitude_dd), performance == "Satisfactory")
+haul_trans = haul
+coordinates(haul_trans) <- c("longitude_dd", "latitude_dd")
+proj4string(haul_trans) <- CRS("+proj=longlat +datum=WGS84")
+newproj = paste("+proj=utm +zone=10 ellps=WGS84")
+#newproj = paste("+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+haul_trans <- spTransform(haul_trans, CRS(newproj))
+haul_trans = as.data.frame(haul_trans)
+haul_trans$longitude_dd = haul_trans$longitude_dd/10000
+haul_trans$latitude_dd = haul_trans$latitude_dd/10000
+haul_trans$year = as.numeric(substr(haul_trans$date_yyyymmdd,1,4))
+haul$X = haul_trans$longitude_dd
+haul$Y = haul_trans$latitude_dd
+haul$year = haul_trans$year
+#haul$year_centered = haul$year - mean(unique(haul$year))
+
+# center and scale depth, removing NAs
+haul = dplyr::filter(haul, !is.na(depth_hi_prec_m))
+haul$log_depth_scaled = scale(log(haul$depth_hi_prec_m))
+haul$log_depth_scaled2 = haul$log_depth_scaled ^ 2
+
+# catch data includes catch, effort, etc. This takes a few minutes to grab all ~ 900 spp
+catch = nwfscSurvey::PullCatch.fn(SurveyName="NWFSC.Combo")
+# format to later join catch and haul
+catch = dplyr::rename(catch, trawl_id = Trawl_id)
+catch$trawl_id = as.numeric(catch$trawl_id)
+
+catch$common_name = NA
+#catch$common_name[which(catch$Scientific_name=="Ophiodon elongatus")] = "lingcod"
+catch$common_name[which(catch$Scientific_name=="Microstomus pacificus")] = "Dover sole"
+catch$common_name[which(catch$Scientific_name=="Sebastolobus alascanus")] = "shortspine thornyhead"
+catch$common_name[which(catch$Scientific_name=="Atheresthes stomias")] = "arrowtooth flounder"
+catch$common_name[which(catch$Scientific_name=="Hippoglossus stenolepis")] = "Pacific halibut"
+catch$common_name[which(catch$Scientific_name=="Glyptocephalus zachirus")] = "rex sole"
+catch$common_name[which(catch$Scientific_name=="Parophrys vetulus")] = "English sole"
+catch$common_name[which(catch$Scientific_name=="Anoplopoma fimbria")] = "sablefish"
+catch$common_name[which(catch$Scientific_name=="Sebastes alutus")] = "Pacific ocean perch"
+catch$common_name[which(catch$Scientific_name=="Gadus macrocephalus")] = "Pacific cod" # few obs
+catch$common_name[which(catch$Scientific_name=="Raja rhina")] = "longnose skate"
+catch$common_name[which(catch$Scientific_name=="Raja binoculata")] = "big skate"
+catch$common_name[which(catch$Scientific_name=="Squalus suckleyi")] = "spiny dogfish"
+catch = dplyr::filter(catch, !is.na(common_name))
+
+# Loop over species
+species = unique(catch$common_name)
+
+for(spp in 1:length(species)) {
+  
+  # filter by species and only include range of coordinates with positive observations over the timeseries
+  subset = dplyr::filter(catch, common_name == species[spp])
+  haul_new = dplyr::left_join(haul, subset) %>%
+    dplyr::filter(latitude >= min(latitude[which(cpue_kg_km2>0)]),
+                  latitude <= max(latitude[which(cpue_kg_km2>0)]),
+                  longitude >= min(longitude[which(cpue_kg_km2)]),
+                  longitude <= max(longitude[which(cpue_kg_km2>0)]))
+  
+  # iterate fits over a range of number of knots,
+  # using AUC and Tweedie predictive density to evaluate performance
+  performance <- data.frame(
+    knots = n_knots,
+    auc = 0, tweedie_dens = 0
+  )
+  
+  for (k in seq_len(nrow(performance))) {
+    
+    # sdmTMB_cv
+    # create ids based on latitude quantiles
+    haul_new$fold = 5
+    haul_new$fold[which(haul_new$lat < quantile(haul_new$lat,0.8))] = 4
+    haul_new$fold[which(haul_new$lat < quantile(haul_new$lat,0.6))] = 3
+    haul_new$fold[which(haul_new$lat < quantile(haul_new$lat,0.4))] = 2
+    haul_new$fold[which(haul_new$lat < quantile(haul_new$lat,0.2))] = 1
+    
+    # Set NA CPUEs to 0
+    haul_new$cpue_kg_km2[which(is.na(haul_new$cpue_kg_km2))] = 0
+    
+    if(use_cv==TRUE) {
+      density_model <- sdmTMB_cv(formula = cpue_kg_km2 ~ log_depth_scaled + log_depth_scaled2 + as.factor(year),
+                                 data = haul_new, x = "X", y = "Y", k_folds=max(haul_new$fold), fold_ids = "fold",
+                                 n_knots = n_knots,
+                                 time = "year", 
+                                 anisotropy = TRUE,
+                                 family = tweedie(link = "log")
+                                 #control = sdmTMBcontrol(step.min = 0.01, step.max = 1)
+                                 )
+      
+      if(!dir.exists(paste0("output/WC/",species[spp]))) dir.create(paste0("output/WC/",species[spp]))
+      saveRDS(density_model, file=paste0("output/WC/",species[spp],"/",species[spp],"_",performance$knots[k],"_density.rds"))
+    } else {
+      c_spde <- make_spde(haul_new$X, haul_new$Y, n_knots = performance$knots[k])
+      density_model <- sdmTMB(formula = cpue_kg_km2 ~ log_depth_scaled + log_depth_scaled2 + as.factor(year),
+                              data = haul_new,
+                              time = "year", 
+                              spde = c_spde, 
+                              anisotropy = TRUE,
+                              family = tweedie(link = "log")
+                              #control = sdmTMBcontrol(step.min = 0.01, step.max = 1)
+                              )
+      
+      if(!dir.exists(paste0("output/WC/",species[spp]))) dir.create(paste0("output/WC/",species[spp]))
+      saveRDS(density_model, file=paste0("output/WC/",species[spp],"/",species[spp],"_",performance$knots[k],"_density.rds"))
+      
+    }
+    
+    # validate against the test set
+    performance[k, "tweedie_dens"] = sum(density_model$data$cv_loglik)
+    
+    saveRDS(performance, file = paste0("output/WC/",species[spp],"_performance.rds"))
+  }
+  
+  # plot Tweedie predictive density as a function of knots. Higher is better.
+  #b <- ggplot(performance, aes(knots, tweedie_dens)) + geom_point() +
+  #  geom_line() + ylab("Tweedie predictive density") + xlab("Knots")
+  
+  #performance_plot <- ggpubr::ggarrange(a, b, ncol = 1, nrow = 2)
+  #ggpubr::ggexport(performance_plot, filename = paste0("output/WC/",species[spp],"_knots.pdf"))
+}
+
+##########################################################################################################
+# make prediction raster roughly from grid_cell centroids, given standard cell dimensions (here in meters, converted from nm)
+
+# read in the grid cell data from the survey design
+grid_cells = readxl::read_excel("data/WC/WC_BTS/shapefiles/survey_grid/Selection Set 2018 with Cell Corners.xlsx")
+coordinates(grid_cells) <- c("Cent.Long", "Cent.Lat")
+proj4string(grid_cells) <- CRS("+proj=longlat +datum=WGS84")
+grid_cells <- spTransform(grid_cells, CRS(newproj))
+
+# make prediction raster roughly from grid_cell centroids, given standard cell dimensions (here in meters, converted from nm)
+predict_raster = raster(grid_cells, resolution = c(2778,3704), vals = NULL)
+## load custom bathymetry raster
+bathy_hiRes <- raster("data/WC/WC_BTS/bathy_clipped")
+bathy_hiRes <- bathy_hiRes / 10 # units were originally decimeters, so convert to meters
+# aggregate and project bathymetry to survey grid cells, the absolute minimum resolution of the prediction grid
+bathy_raster <- projectRaster(bathy_hiRes, predict_raster, crs = newproj, method="bilinear")
+# load Cowcod Conservation Areas, not included in trawl survey, and reproject
+CCA = rgdal::readOGR('data/WC/WC_BTS/shapefiles/spatial_closure_boundaries/kv299cy7357.shp')
+CCA = sp::spTransform(CCA, sp::CRS(newproj))
+# mask CCA from bathymetry raster used for prediction
+bathy_raster = raster::mask(bathy_raster, CCA, inverse = TRUE)
+# create matrix of point data with coordinates and depth from raster
+wc_grid <- as.data.frame(rasterToPoints(bathy_raster))
+colnames(wc_grid) = c("X", "Y", "depth")
+
+# scale covariates
+wc_grid$log_depth_scaled <- (log(wc_grid$depth * -1) - mean(log(haul$depth_hi_prec_m))) / sd(log(haul$depth_hi_prec_m))
+wc_grid$log_depth_scaled2 <- wc_grid$log_depth_scaled ^ 2
+wc_grid$X <- wc_grid$X/10000
+wc_grid$Y <- wc_grid$Y/10000
+
+saveRDS(wc_grid, file=paste0("data/WC/WC_BTS/wc_grid.rds")) # save prediction grid
