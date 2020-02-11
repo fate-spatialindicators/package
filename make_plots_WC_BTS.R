d51cd1ac4476a09696627d988d69fbd3198482a2
Lewis Barnett <labarnett@ucdavis.edu>
2020-02-11 14:23:48 -0800

    change panel layout to 5x2 given 10 species

--- make_plots_WC_BTS.R
+++ make_plots_WC_BTS.R
@@ -114,7 +114,7 @@ for(spp in 1:length(species)) {
     ggtitle(paste0(species[spp],"_COG_Inertia"))
   
   # global index of collocation, comparing all years to individual years
-  if(!(spp %in% c(9:11))){
+  if(!(spp %in% c(5:10))){
     gic_plots[[spp]] <- p %>% 
       group_by(year) %>% 
       summarise(gic=gic(x1=X,
@@ -148,7 +148,7 @@ for(spp in 1:length(species)) {
   p_All = predict(d, newdata=Predict_data_years, return_tmb_object = TRUE)  
   COG = get_cog(p_All)
   # add x axis labels and legend only to specific panels to be combined later
-  if(!(spp %in% c(9:11))){
+  if(!(spp %in% c(5:10))){
     COG_plots_N[[spp]] = ggplot(filter(COG, coord == "Y"), aes(year, est)) +
       geom_ribbon(aes(ymin = lwr, ymax = upr), fill = "grey70") +
       geom_line(color = "black") +
@@ -168,7 +168,7 @@ for(spp in 1:length(species)) {
       scale_x_continuous(breaks=seq(2005, 2015, 5))
   }
   
-  if(!(spp %in% c(9:11))){
+  if(!(spp %in% c(5:10))){
     COG_plots_E[[spp]] = ggplot(filter(COG, coord == "X"), aes(year, est)) +
       geom_ribbon(aes(ymin = lwr, ymax = upr), fill = "grey70") +
       geom_line(color = "black") +
@@ -240,7 +240,7 @@ ggsave(filename = "figures/WC/WC_BTS/crosses.pdf",
        plot = marrangeGrob(grobs = mycgi_cross_plots, nrow = 1, ncol = 1),
        width = 7, height = 7, units = c("in"))
 ggsave(filename = "figures/WC/WC_BTS/ellipses.pdf",
-       plot = arrangeGrob(grobs = mycgi_ellipse_plots, ncol = 4, bottom = "Eastings (km)",
+       plot = arrangeGrob(grobs = mycgi_ellipse_plots, ncol = 5, bottom = "Eastings (km)",
                           left = grid::textGrob("Northings (km)", rot = 90, vjust = 0.2)),
        width = 7, height = 9, units = c("in"))
 # plot legend separately for now
@@ -254,17 +254,17 @@ grid::grid.draw(legend)
 dev.off()
 # plot GIC by year
 ggsave(filename = "figures/WC/WC_BTS/GIC.pdf",
-       plot = arrangeGrob(grobs = gic_plots, ncol = 4, bottom = "Year",
+       plot = arrangeGrob(grobs = gic_plots, ncol = 5, bottom = "Year",
                           left = grid::textGrob("Global index of collocation", rot = 90, vjust = 0.2)),
        width = 9, height = 7, units = c("in"))
 
 # COG timeseries plots from model output
 ggsave(filename = "figures/WC/WC_BTS/COG_model_est_N.pdf",
-       plot = arrangeGrob(grobs = COG_plots_N, ncol = 4, bottom = "Year",
+       plot = arrangeGrob(grobs = COG_plots_N, ncol = 5, bottom = "Year",
                           left = grid::textGrob("COG Northings (km)", rot = 90, vjust = 0.2)),
        width = 12, height = 8, units = c("in"))
 ggsave(filename = "figures/WC/WC_BTS/COG_model_est_E.pdf",
-       plot = arrangeGrob(grobs = COG_plots_E, ncol = 4, bottom = "Year",
+       plot = arrangeGrob(grobs = COG_plots_E, ncol = 5, bottom = "Year",
                           left = grid::textGrob("COG Eastings (km)", rot = 90, vjust = 0.2)),
        width = 12, height = 8, units = c("in"))
 
@@ -281,11 +281,11 @@ ggsave(filename = "figures/WC/WC_BTS/st_maps.pdf",
 ## model checking plots
 # anisotropy plots for each species
 ggsave(filename = "figures/WC/WC_BTS/anisotropy.pdf",
-       plot = arrangeGrob(grobs = anisotropy_plots, ncol = 4),
+       plot = arrangeGrob(grobs = anisotropy_plots, ncol = 5),
        width = 12, height = 8, units = c("in"))
 # qq norm plots for each species
 pdf(file = "figures/WC/WC_BTS/qqnorm.pdf", width = 12, height = 12)
-par(mfrow=c(3,4))
+par(mfrow=c(2,5))
 for(spp in 1:length(species)) {
   plot(qq_plots[[spp]], pch = ".", main = paste0(species[spp],"_qq")); abline(a = 0, b = 1)
 }
